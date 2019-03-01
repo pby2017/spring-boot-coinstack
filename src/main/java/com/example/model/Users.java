406c1b054146938ab68ec199a0992410229216ba
ByungYongPark <pby2017@github.com>
2019-03-01 22:38:10 +0900

    API 호출에 대해 JSON 형식으로 응답하기 위해 생성

---
+++ src/main/java/com/example/model/Users.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class Users {
+	
+	private String privateKey;
+	private String address;
+	
+	public String getPrivateKey() {
+		return privateKey;
+	}
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+		
+}
